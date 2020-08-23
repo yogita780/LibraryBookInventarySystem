019174c66d975713e884e0a44b52598ba65c2cc8
Yogita Balyan <yogita.balyan_cs19@gla.ac.in>
2020-08-23 20:33:26 +0530

    create getter and setter methods

--- src/definitations/Book.java
+++ src/definitations/Book.java
@@ -12,4 +12,27 @@ public class Book {
     private String authorName;
     private String ISBNNumber;
 
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getISBNNumber() {
+        return ISBNNumber;
+    }
+
+    public void setISBNNumber(String ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
 }
