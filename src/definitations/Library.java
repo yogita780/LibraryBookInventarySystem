58c689e7c3b1609b66b1212f8505e850c034df6b
Yogita Balyan <yogita.balyan_cs19@gla.ac.in>
2020-08-29 21:22:23 +0530

    add constructor,getter and setter method to Library.java

--- src/definitations/Library.java
+++ src/definitations/Library.java
@@ -9,4 +9,22 @@ package definitations;
 
 public class Library {
     private Book[] currentlyBooksAvailable;
+
+    public Library() {
+        Book book1 = new Book("Operating System Concept", "Avi Silberschaztz", "3456723456894");
+        Book book2 = new Book("Higher Mathematics", "B.S Grewal", "2354613789453");
+        Book book3 = new Book("Chandrakanta", "Devaki Nandan Khatri", "4532167834567");
+        Book[] books = new Book[]{book1, book2, book3};
+        this.currentlyBooksAvailable = books;
+
+    }
+
+    public Book[] getCurrentlyBooksAvailable() {
+        return currentlyBooksAvailable;
+    }
+
+    public void setCurrentlyBooksAvailable(Book[] currentlyBooksAvailable) {
+        this.currentlyBooksAvailable = currentlyBooksAvailable;
+    }
+
 }
