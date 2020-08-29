19c9b0ca849fc74254df6c91fb848c480249b191
Yogita Balyan <yogita.balyan_cs19@gla.ac.in>
2020-08-29 19:58:36 +0530

    add getter and setter methods

--- src/definitations/Student.java
+++ src/definitations/Student.java
@@ -13,4 +13,44 @@ public class Student {
     private String lastNameOfStudent;
     private long universityRollNumber;
     private int numberOfIssuedBook;
+
+    public String getFirstNameOfStudent() {
+        return firstNameOfStudent;
+    }
+
+    public void setFirstNameOfStudent(String firstNameOfStudent) {
+        this.firstNameOfStudent = firstNameOfStudent;
+    }
+
+    public String getMiddleNameOfStudent() {
+        return middleNameOfStudent;
+    }
+
+    public void setMiddleNameOfStudent(String middleNameOfStudent) {
+        this.middleNameOfStudent = middleNameOfStudent;
+    }
+
+    public String getLastNameOfStudent() {
+        return lastNameOfStudent;
+    }
+
+    public void setLastNameOfStudent(String lastNameOfStudent) {
+        this.lastNameOfStudent = lastNameOfStudent;
+    }
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
+    public int getNumberOfIssuedBook() {
+        return numberOfIssuedBook;
+    }
+
+    public void setNumberOfIssuedBook(int numberOfIssuedBook) {
+        this.numberOfIssuedBook = numberOfIssuedBook;
+    }
 }
