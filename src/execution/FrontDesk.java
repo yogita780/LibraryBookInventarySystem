9b481c2fc9ac4c56882dfc6e23b3a734c02d108f
Yogita Balyan <yogita.balyan_cs19@gla.ac.in>
2020-08-30 09:02:35 +0530

    object created for book

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -7,10 +7,26 @@
 
 package execution;
 
+import definitations.Book;
+import definitations.Library;
+import definitations.Student;
+
 import java.util.Scanner;
 
 public class FrontDesk {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scannerObject = new Scanner(System.in);
+        Book bookobject = new Book();
+        Library libraryObject = new Library();
+        Student studentObject = new Student();
+        System.out.println("enter your first name");
+        String firstname = scannerObject.nextLine();
+        System.out.println("enter your middle name");
+        String middle = scannerObject.nextLine();
+        System.out.println("enter your last name");
+        String lastname = scannerObject.nextLine();
+        System.out.println("enter your roll.no:");
+        long rollNumber = scannerObject.nextLong();
+        scannerObject.nextLine();
     }
 }
